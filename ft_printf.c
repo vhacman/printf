4ce8e185d3c4cdf150f125ce7a48949357c306ba
gabdev98 <gabdev98@MSI.>
2025-02-05 14:29:27 +0100

    ricominciato

--- ft_printf.c
+++ ft_printf.c
@@ -10,5 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.c"
+#include "printf.h"
 
+int	handle_conversion(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (specifier == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (specifier == 'p')
+		return (ft_putptr(va_arg(args, void *)));
+	else if (specifier == 'd' || specifier == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (specifier == 'u')
+		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
+	else if (specifier == x)
+		return (ft_putnbr_hex(va_arg(args, unsigned int), 0));
+	else if (specifier == X)
+		return ((ft_putnbr_hex(va_arg, unsigned int), 1));
+	else if (specifier == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '\0')
+		{
+			i++;
+			count += hande_conversion(format[i], args);
+		}
+		else
+			count += hande_conversion(format[i]);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
