e10814169287b9e0819c82f97beaed25f447af69
gabdev98 <gabdev98@MSI.>
2025-02-05 16:52:18 +0100

    da testare

--- Makefile
+++ Makefile
@@ -6,51 +6,45 @@
 #    By: vhacman <vhacman@student.42roma.it>        +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/04 08:27:30 by vhacman           #+#    #+#              #
-#    Updated: 2025/02/05 15:46:50 by vhacman          ###   ########.fr        #
+#    Updated: 2025/02/05 16:22:55 by vhacman          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#definizione delle variabili
 NAME = libftprintf.a
 CC = gcc
 CFLAGS = -Wall -Werror -Wextra
 RM = rm -f
+AR = ar
+ARFLAGS = rcs
 
-AR = ar 
-ARFLAGS = rcs 
-
-#definizione delle cartelle
+# Definizione delle cartelle
 SRC_DIR = src/
 OBJ_DIR = obj/
 
-#lista file sorgente
-SRC = 
-SRCS = $(addsuffix .c, $(SRC))
-OBJS = $(SRCS:.c=.o)
+# Lista file sorgente
+SRC = ft_printf ft_utils ft_putnbr_hex
+SRCS = $(addprefix $(SRC_DIR), $(SRC:=.c))
+OBJS = $(SRCS:$(SRC_DIR)%.c=$(OBJ_DIR)%.o)
 
-SRCS := $(addprefix $(SRC_DIR)/, $(SRCS))
-OBJS := $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
-
-#target
+# Target
 all: $(NAME)
 
-#crea libreria statica (rcs= sostituisce i file .o gia' esistenti, 
-#crea archivio se non esiste, aggiunge un indice per velocizzare il linking)
+# Crea libreria statica
 $(NAME): $(OBJS)
 	ar rcs $(NAME) $(OBJS)
 
-#questa regola compila un file .c in un file .o all'interno della directory 
-#$(OBJ_DIR) e assicura che la directory di destinazione esista prima della compilazione.
+# Compila un file .c in un file .o
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c | $(OBJ_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-#creazione cartella obj/ per organizzare i file .o, se non esiste
+# Crea la cartella obj/ se non esiste
 $(OBJ_DIR):
 	mkdir -p $(OBJ_DIR)
 
-#pulizia
-clean: 
+# Pulizia
+clean:
 	$(RM) $(OBJS)
+
 fclean: clean
 	$(RM) $(NAME)
 
--- ft_printf.h
+++ ft_printf.h
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*															 */
-/*											 :::	 ::::::::   */
-/*   ft_printf.h								:+:	 :+:    :+:   */
-/*										  +:+ +:+	    +:+	*/
-/*   By: vhacman <vhacman@student.42roma.it>	   +#+  +:+	  +#+	   */
-/*									   +#+#+#+#+#+   +#+		 */
-/*   Created: 2025/02/05 14:33:10 by vhacman		 #+#    #+#		   */
-/*   Updated: 2025/02/05 14:33:10 by vhacman		###   ########.fr	  */
-/*															 */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vhacman <vhacman@student.42roma.it>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 14:33:10 by vhacman           #+#    #+#             */
+/*   Updated: 2025/02/05 17:20:00 by vhacman          ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
@@ -21,11 +21,13 @@
 # include <stdio.h>
 # include <stdarg.h>
 
-int	t_printf(const char *format, ...);
-int	ft_putchar(char c);
-int	ft_putnbr(int n);
-int	ft_putstr(char *str);
-int	ft_putnbr_unsigned(unsigned int n);
-int	ft_putnbr_hex(unsigned int n, int upper);
-int	ft_putptr(void *ptr);
+int     ft_printf(const char *format, ...);
+int     ft_putchar(char c);
+int     ft_putnbr(int n);
+int     ft_putstr(char *str);
+int     ft_putnbr_unsigned(unsigned int n);
+int     ft_putnbr_hex(unsigned int n, int upper);
+int     ft_putptr(void *ptr);
+int	handle_conversion(char specifier, va_list args);
+
 #endif
